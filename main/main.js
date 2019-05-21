98737c1ce28804885460acc38c17533601403edf
woniu <18810913096@163.com>
2019-05-21 23:04:50 +0800

    complete test

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var price;
+    if (inputs.distance <= 2) {
+        price=6+inputs.parkTime*0.25;
+
+    } else if (inputs.distance > 2 && inputs.distance <= 8) {
+        price=6+(inputs.distance-2)*0.8+inputs.parkTime*0.25;
+    } else {
+        price=6+(8-2)*0.8+(inputs.distance-8)*1.2+inputs.parkTime*0.25;
+    }
+
+    return Math.round(price);
 };
